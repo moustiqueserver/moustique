6e4cd84026b676c4a530827a4e7c063792f89f1d
moustiqueserver <mulf@protonmail.com>
2025-12-25 13:02:28 +0100

    Fixing client data in admin-page

--- broker.go
+++ broker.go
@@ -592,12 +592,17 @@ func (b *Broker) GetClients() []*Client {
 	b.mu.RLock()
 	defer b.mu.RUnlock()
 
-	// Return all clients that have message queues (active clients)
-	clients := make([]*Client, 0, len(b.clients))
-	for clientName, client := range b.clients {
-		// Only include clients that have a message queue (active)
-		if _, hasQueue := b.messageQueue[clientName]; hasQueue {
+	// Use messageQueue to find active clients (those with queues)
+	// and return their Client info if available
+	clients := make([]*Client, 0, len(b.messageQueue))
+	for clientName := range b.messageQueue {
+		if client, exists := b.clients[clientName]; exists {
 			clients = append(clients, client)
+		} else {
+			// Fallback: create minimal client info if not in clients map
+			clients = append(clients, &Client{
+				Name: clientName,
+			})
 		}
 	}
 	return clients
