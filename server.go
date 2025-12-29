329f04747d150dc7f96cc28eaf637f98ded33858
moustiqueserver <mulf@protonmail.com>
2025-12-29 01:16:23 +0100

    Allowing [] in topics

--- server.go
+++ server.go
@@ -803,7 +803,7 @@ func (s *Server) validateInput(topic, message string) error {
 	// Validate topic characters (alphanumeric, underscore, hyphen, dot, slash)
 	for _, c := range topic {
 		if !((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') ||
-			(c >= '0' && c <= '9') || c == '_' || c == '-' || c == '.' || c == '/' || c == ' ' || c == ':' || c == 'è' || c == 'é') {
+			(c >= '0' && c <= '9') || c == '_' || c == '-' || c == '.' || c == '/' || c == ' ' || c == ':' || c == 'è' || c == 'é' || c == '[' || c == ']') {
 			return fmt.Errorf("topic %s contains invalid characters", topic)
 		}
 	}
@@ -826,7 +826,7 @@ func (s *Server) validateSubscribeTopic(topic string) error {
 	// Validate topic characters (allow +, *, and # for wildcards)
 	for _, c := range topic {
 		if !((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') ||
-			(c >= '0' && c <= '9') || c == '_' || c == '-' || c == '.' || c == '/' || c == '+' || c == '*' || c == '#' || c == ' ' || c == ':' || c == 'è' || c == 'é') {
+			(c >= '0' && c <= '9') || c == '_' || c == '-' || c == '.' || c == '/' || c == '+' || c == '*' || c == '#' || c == ' ' || c == ':' || c == 'è' || c == 'é' || c == '[' || c == ']') {
 			return fmt.Errorf("topic %s contains invalid characters", topic)
 		}
 	}
